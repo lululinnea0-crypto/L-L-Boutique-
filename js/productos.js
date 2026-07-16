783a4edea0220c0548de1d8c7377d92df012c7e5
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-16 20:40:34 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,1 +1,17 @@
+const productos = [
 
+{
+    id:1,
+    nombre:"Cardigan Largo",
+    precio:39990,
+    imagen:"img/productos/cardigan.jpg"
+},
+
+{
+    id:2,
+    nombre:"Pantalón Sastrero",
+    precio:49990,
+    imagen:"img/productos/pantalon.jpg"
+}
+
+];
