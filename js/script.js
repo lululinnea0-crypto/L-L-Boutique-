248e168c75bfbc6cf0f91d6efb7f21907eb29564
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-16 20:51:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,33 @@ const contenedor = document.getElementById("contenedor-productos");
 
 productos.forEach(producto => {
 
+    let tallesHTML = "";
+
+    for (let color in producto.talles){
+
+        tallesHTML += `
+            <p><strong>${color}</strong>: ${producto.talles[color]}</p>
+        `;
+
+    }
+
     contenedor.innerHTML += `
 
     <div class="producto">
 
         <img src="${producto.imagen}" alt="${producto.nombre}">
 
-        <h3>${producto.nombre}</h3>
+        <h3>💌 ${producto.nombre}</h3>
 
         <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
+
+        <div class="talles">
+
+            <h4>💞 Talles</h4>
+
+            ${tallesHTML}
+
+        </div>
 
         <button>Reservar</button>
 
