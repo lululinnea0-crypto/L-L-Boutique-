4324e49f80989eae38af62d1a418bb83651fe55d
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-16 20:51:06 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -2,10 +2,28 @@ const productos = [
 
 {
     id:1,
-    nombre:"Cardigan Largo",
+
+    nombre:"Suéter de manga larga con cuello en V, de punto trenzado",
+
     precio:39990,
-    imagen:"img/productos/cardigan.jpg"
-},
+
+    imagen:"img/productos/cardigan.jpg",
+
+    talles:{
+
+        "🖤 Negro":"XXS al L",
+
+        "🩶 Gris":"XXS al L",
+
+        "🤍 Blanco":"XXS al XL",
+
+        "❤️ Rojo":"XS al XL",
+
+        "🤎 Marrón":"XS al XL",
+
+        "🔵 Azul":"XXS al L"
+
+    }
 
 {
     id:2,
