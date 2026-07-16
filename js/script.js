8d68d28660dd24b2ce8793ed989f4e6b6d507184
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-16 20:45:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const contenedor = document.getElementById("contenedor-productos");
 
+productos.forEach(producto => {
+
+    contenedor.innerHTML += `
+
+    <div class="producto">
+
+        <img src="${producto.imagen}" alt="${producto.nombre}">
+
+        <h3>${producto.nombre}</h3>
+
+        <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
+
+        <button>Reservar</button>
+
+    </div>
+
+    `;
+
+});
