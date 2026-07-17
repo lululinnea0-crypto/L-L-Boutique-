de397e3df731c206a0db5af1b00f101524a051d9
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-16 21:02:55 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -33,3 +33,15 @@ const productos = [
 }
 
 ];
+
+{
+    id: 1,
+    nombre: "Suéter de manga larga con cuello en V",
+    precio: 39990,
+    categoria: "Cardigans",
+    imagen: "img/productos/cardigan.jpg",
+    talles: {
+        "Negro": "XXS al L",
+        "Blanco": "XXS al XL"
+    }
+}
