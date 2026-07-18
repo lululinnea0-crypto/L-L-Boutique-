65b1793ab9fffddc74b2a8d01dc641038735a57c
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-18 01:35:58 -0300

    Update producto.js

--- productos/producto.js
+++ productos/producto.js
@@ -1,25 +1,100 @@
-console.log("producto.js cargado");
-
 const parametros = new URLSearchParams(window.location.search);
 const id = parametros.get("id");
 
-console.log("ID recibido:", id);
-
 const producto = productos.find(p => p.id == id);
-
-console.log("Producto encontrado:", producto);
 
 if (producto) {
 
+    let tallesHTML = "";
+
+    for (let talle in producto.talles) {
+        tallesHTML += `
+            <option value="${talle}">
+                ${talle}: ${producto.talles[talle]}
+            </option>
+        `;
+    }
+
     document.getElementById("producto").innerHTML = `
-        <img src="../${producto.imagen}" style="max-width:300px;width:100%">
 
-        <h2>${producto.nombre}</h2>
+    <img src="../${producto.imagen}" 
+    alt="${producto.nombre}" 
+    style="max-width:300px;width:100%;">
 
-        <p>Precio: $${producto.precio.toLocaleString("es-AR")}</p>
+    <h2>${producto.nombre}</h2>
 
-        <p>Producto cargado correctamente</p>
+    <p><strong>Precio:</strong> 
+    $${producto.precio.toLocaleString("es-AR")}</p>
+
+    <h3>Talle</h3>
+
+    <select id="talle">
+        ${tallesHTML}
+    </select>
+
+
+    <h3>Color</h3>
+
+    <select id="color">
+        ${Object.keys(producto.talles).map(color => 
+        `<option>${color}</option>`).join("")}
+    </select>
+
+
+    <h3>Forma de pago</h3>
+
+    <label>
+        <input type="radio" name="pago" value="100" checked>
+        Pagar el 100%
+    </label>
+
+    <br>
+
+    <label>
+        <input type="radio" name="pago" value="50">
+        Reservar con el 50%
+    </label>
+
+
+    <h3>Hoy pagás</h3>
+
+    <p id="pago-hoy">
+    $${producto.precio.toLocaleString("es-AR")}
+    </p>
+
+
+    <button onclick="agregarAlCarrito()">
+        🛒 Agregar al carrito
+    </button>
+
     `;
+
+
+    document.querySelectorAll('input[name="pago"]')
+    .forEach(opcion => {
+
+        opcion.addEventListener("change", () => {
+
+            const pagoHoy = document.getElementById("pago-hoy");
+
+            if(opcion.value == "50"){
+
+                pagoHoy.textContent =
+                "$" + (producto.precio / 2)
+                .toLocaleString("es-AR");
+
+            } else {
+
+                pagoHoy.textContent =
+                "$" + producto.precio
+                .toLocaleString("es-AR");
+
+            }
+
+        });
+
+    });
+
 
 } else {
 
