9c511ea14ac369230d38814ca21e9633ec7e4401
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-17 21:36:29 -0300

    Update producto.js

--- productos/producto.js
+++ productos/producto.js
@@ -1,1 +1,8 @@
+// Obtener el ID del producto desde la URL
+const parametros = new URLSearchParams(window.location.search);
+const id = parametros.get("id");
 
+document.getElementById("producto").innerHTML = `
+    <h2>Producto ${id}</h2>
+    <p>Esta página todavía está en construcción.</p>
+`;
