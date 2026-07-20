b77b04fc0a7129182cffdd2b7a622dca70f2c6e3
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-20 01:25:35 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,24 +1,27 @@
 const productos = [
-    {
-        id: 1,
-        nombre: "Suéter de manga larga con cuello en V, de punto trenzado",
-        precio: 39990,
-        imagen: "img/productos/cardigan.jpg",
-        colores: {
-    "Negro": ["S", "M", "L", "XL", "XXL"],
-    "Blanco": ["XS", "S", "M", "L", "XL"],
-    "Gris": ["S", "M", "L"]
-}
-    },
+  {
+    id: 1,
+    nombre: "Suéter de manga larga con cuello en V, de punto trenzado",
+    precio: 39990,
+    imagen: "img/productos/cardigan.jpg",
+    etiqueta: "✨ NUEVO",
+    colores: {
+        "🖤 Negro": ["XXS","XS","S","M","L"],
+        "🩶 Gris": ["XXS","XS","S","M","L"],
+        "🤍 Blanco": ["XXS","XS","S","M","L","XL"]
+    }
+},
 
     {
         id: 2,
         nombre: "Pantalón Sastrero",
         precio: 49990,
         imagen: "img/productos/pantalon.jpg",
+        etiqueta: "🔥 MÁS VENDIDO",
         colores: {
     "Negro": ["S", "M", "L", "XL"],
     "Beige": ["S", "M", "L", "XL"]
         }
     }
 ];
+
