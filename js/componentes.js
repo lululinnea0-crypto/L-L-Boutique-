ef2fb37c48d992d65198d90d2ccc4cb972602e24
lululinnea0-crypto <lululinnea0@gmail.com>
2026-08-02 15:44:46 -0300

    Update componentes.js

--- js/componentes.js
+++ js/componentes.js
@@ -1,1 +1,23 @@
+// ===============================
+// Cargar componentes
+// ===============================
 
+function cargarHeader(){
+
+    const contenedor = document.getElementById("contenedor-header");
+
+    if(!contenedor) return;
+
+
+    fetch("componentes/header.html")
+    .then(res => res.text())
+    .then(data => {
+
+        contenedor.innerHTML = data;
+
+    });
+
+}
+
+
+cargarHeader();
