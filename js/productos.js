2f426e45aadddb5c43e2278a39baf898766aa0da
lululinnea0-crypto <lululinnea0@gmail.com>
2026-08-02 10:53:01 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,40 +1,22 @@
 const productos = [
 
-    // ==========================
-    // MODA
-    // ==========================
-    {
-    id: 2,
+{
+    id: 1,
     nombre: "Suéter Cheshire",
     categoria: "sueters",
+
     precio: 33000,
-    imagen: "../img/productos/sueters/Sueter Cheshire/1.jpg"
+
+    imagen: "img/productos/sueters/Sueter Cheshire/1.jpg",
 
     carpeta: "Sueter Cheshire",
+    cantidadImagenes: 3,
 
     tipo: "pedido",
 
     colores: {
-        "💗 Rosa": ["S","M","L"]
+        "💗 Rosa": ["S", "M", "L"]
     }
-        
-    },
-
-    // ==========================
-    // BELLEZA
-    // ==========================
-    {
-        id: 50,
-        nombre: "KAIAK SONAR EDT MASC 100ML LTM",
-        categoria: "perfumes",
-        precio: 28990,
-
-        carpeta: "KAIAK SONAR EDT MASC 100ML LTM",
-       imagenes: "../img/productos/perfumes/KAIAK SONAR EDT MASC 100ML LTM/1.jpg"
-
-        tipo: "stock",
-
-        etiqueta: "🌿 Natura"
-    }
+}
 
 ];
